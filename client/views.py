9249519ab7e820b452003bf7c258b1e80008b3ae
Steve McGonigal <steve.mcgonigal@cottages-and-castles.co.uk>
2017-05-12 10:10:32 +0100

    login

--- client/views.py
+++ client/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home_page(request):
+    return render(request, 'client/home_page.html', {})
